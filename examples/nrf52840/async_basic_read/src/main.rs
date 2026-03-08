3cfd2a4980025f6b489d6d5aee6922d223b4c85d
Quentin <quentin@rasmont.com>
2026-03-08 21:35:57 +0100

    Add an async basic read example using embassy

---
+++ examples/nrf52840/async_basic_read/src/main.rs
@@ -0,0 +1,61 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_nrf::twim::{self, Twim};
+use embassy_nrf::{bind_interrupts, peripherals};
+use embassy_time::{Timer, Delay};
+use static_cell::ConstStaticCell;
+use {defmt_rtt as _, panic_probe as _};
+use defmt::info;
+
+use bmi2::config;
+use bmi2::Bmi2;
+use bmi2::{types::Burst, types::PwrCtrl, I2cAddr};
+
+bind_interrupts!(struct Irqs {
+    TWISPI0 => twim::InterruptHandler<peripherals::TWISPI0>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+
+    info!("Initializing TWI...");
+    let config = twim::Config::default();
+    static RAM_BUFFER: ConstStaticCell<[u8; 16]> = ConstStaticCell::new([0; 16]);
+    let twi = Twim::new(p.TWISPI0, Irqs, p.P0_03, p.P0_04, config, RAM_BUFFER.take());
+    let delay = Delay;
+
+    const BUFFER_SIZE: usize = 256;
+    let mut bmi = Bmi2::<_, _, BUFFER_SIZE>::new_i2c(twi, delay, I2cAddr::Alternative, Burst::new(255));
+
+    let chip_id = bmi.get_chip_id().await.unwrap();
+    info!("chip id: {}", chip_id);
+
+    bmi.init(&config::BMI270_CONFIG_FILE).await.unwrap();
+
+    // Enable power for the accelerometer and the gyroscope.
+    let pwr_ctrl = PwrCtrl {
+        aux_en: false,
+        gyr_en: true,
+        acc_en: true,
+        temp_en: false,
+    };
+    bmi.set_pwr_ctrl(pwr_ctrl).await.unwrap();
+
+    loop {
+        let data = bmi.get_data().await.unwrap();
+        defmt::info!(
+            "data: acc_x:{}, acc_y:{}, acc_z:{}, gyr_x:{}, gyr_y:{}, gyr_z:{}",
+            data.acc.x,
+            data.acc.y,
+            data.acc.z,
+            data.gyr.x,
+            data.gyr.y,
+            data.gyr.z
+        );
+
+        Timer::after_millis(500).await;
+    }
+}
